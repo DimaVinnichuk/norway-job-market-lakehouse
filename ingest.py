cd64ed21d648c660605704692fc864c1e5fdd9b3
DimaVinnichuk <1dima.vinnichuk@gmail.com>
2026-06-21 00:55:28 +0200

    feat(ingest): implement vacancy ingestion and pagination pipeline
    - Refactor function arguments to leverage global constants
    - Implement dynamic pagination loop for automated vacancy extraction
    - Ensure robust error handling and explicit exit codes upon failures
    - Save atomic incremental state info upon successful execution

--- ingest.py
+++ ingest.py
@@ -4,12 +4,14 @@ from logging_config import setup_logging
 import logging
 import requests
 import json
+from urllib.parse import urljoin
 
 setup_logging()
 logger = logging.getLogger(__name__)
 
 URL_TOKEN = "https://pam-stilling-feed.nav.no/api/publicToken"
 URL_VACANCY = "https://pam-stilling-feed.nav.no/api/v1/feed"
+DOMAIN = "https://pam-stilling-feed.nav.no/"
 
 STATUS_FILE_PATH = Path("data/pipeline_state.txt")
 
@@ -22,26 +24,26 @@ def generate_history_point():
     logger.debug("Created date point: %s", thirty_days_ago)
     return thirty_days_ago
 
-def get_pipeline_status(file_path, get_date):
+def get_pipeline_status(get_date):
     """Checks for pipeline status file existence, reading its data or falling back to default."""
 
     logger.info("Checking pipeline execution status...")
-    if file_path.exists():
-        logger.info("Pipeline status file found at %s. Reading existing state.", file_path)
-        content = file_path.read_text(encoding="utf-8").strip()
+    if STATUS_FILE_PATH.exists():
+        logger.info("Pipeline status file found at %s. Reading existing state.", STATUS_FILE_PATH)
+        content = STATUS_FILE_PATH.read_text(encoding="utf-8").strip()
         logger.debug("Retrieved date from status file: %s", content)
         return content
     else:
-        logger.info("Pipeline status file NOT found at %s. Triggering historical backfill mode.", file_path)
+        logger.info("Pipeline status file NOT found at %s. Triggering historical backfill mode.", STATUS_FILE_PATH)
         backfill_point = get_date()
         return backfill_point
 
-def get_jwt_token(url_token):
+def get_jwt_token():
     """Requests and returns a public token from the NAV API"""
 
     try:
         logger.info("Requesting public token...")
-        token_response = requests.get(url_token, timeout=10)
+        token_response = requests.get(URL_TOKEN, timeout=10)
         token_response.raise_for_status()
         try:
             token = token_response.text.split(":")[-1].strip()
@@ -52,61 +54,70 @@ def get_jwt_token(url_token):
                 raise ValueError("Invalid JWT token structure received from API")
         except ValueError as e:
             logger.critical("Token parsing error %s", e)
-            raise SystemExit(e)
+            raise SystemExit(1)
 
     except requests.exceptions.RequestException as e:
         logger.critical("Error retrieving token: %s.", e)
-        raise SystemExit(e)
+        raise SystemExit(1)
 
-def get_vacancy_data(url, header):
-    # TODO: Finalize data extraction logic to fetch full-size data. Add validation.
-    """Requests and returns JSON data about vacancies."""
+def fetch_and_save_vacancy_data(token, time_point):
+    """Fetch and save vacancy data"""
 
-    logger.info("Requesting API to retrieve data...")
-    try:
-        response = requests.get(url, headers=header)
-        response.raise_for_status()
-        json_vacancy_data = response.json()
+    header_data = {
+        "Authorization": f"Bearer {token}",
+        "If-Modified-Since": f"{time_point}"
+    }
+    logger.info("Fetching vacancy data from API...")
+    start_timestamp = datetime.now(timezone.utc)
+    timestamp_filename = start_timestamp.strftime("%d%m%y-%H%M%S")
+    timestamp_for_update_state = start_timestamp.strftime("%a, %d %b %Y %H:%M:%S GMT")
+    page_url = URL_VACANCY
+    page_number = 1
 
-        logger.info("Data successfully retrieved.")
-        return json_vacancy_data
-    except requests.exceptions.RequestException as e:
-        logger.critical("Failed to retrieve data from API: %s.", e)
-        raise SystemError(e)
+    while page_url:
+        data_filename = f"{timestamp_filename}-page{page_number}-raw_jobs.json"
+        path_to_data = Path("data/bronze/") / data_filename
+        path_to_data.parent.mkdir(parents=True, exist_ok=True)
 
-def save_vacancy_data(vacancy_data):
-    # TODO: Implement saving each data page into a separate file.
-    """Saves vacancy JSON data to a file"""
+        try:
+            logger.debug("Requesting URL: %s...", page_url)
+            response = requests.get(page_url, headers=header_data, timeout=10)
+            response.raise_for_status()
+            data_json = response.json()
+        except requests.exceptions.RequestException as e:
+            logger.critical("Failed to retrieve data from API: %s.", e)
+            raise SystemExit(1)
+        
+        if data_json.get("items"):
+            next_url = data_json.get("next_url")
+            with open(path_to_data, "w", encoding="utf-8") as f:
+                json.dump(data_json, f, indent=4)
+            logger.debug("Retrieved page saved to %s.", path_to_data)
+            page_number+=1
+            if next_url:
+                page_url = urljoin(DOMAIN, next_url)
+            else:
+                logger.info("Next URL is missing. Pagination finished.")
+                break
+        else:
+            logger.info("No vacancy data found in the response")
+            break
 
-    path_to_file = Path("data/bronze/vacancy_data.json")
-    path_to_file.parent.mkdir(parents=True, exist_ok=True)
-
-    with open(path_to_file, "w", encoding="utf-8") as f:
-        json.dump(vacancy_data, f, indent=4)
-
-    if path_to_file.exists:
-        logger.info("Data successfully saved")
-    else:
-        logger.error("Error: Failed to create data file")
+    logger.info("Vacancy data ingestion completed successfully.")
+    STATUS_FILE_PATH.parent.mkdir(parents=True, exist_ok=True)
+    STATUS_FILE_PATH.write_text(timestamp_for_update_state, encoding="utf-8")
+    logger.debug("Pipeline state file updated with timestamp: %s", timestamp_for_update_state)
 
 ### main execution
 
 def run_ingestion():
     """Launch ingestion process"""
 
-    logger.info("Start ingestion process")
-    defined_datetime_point = get_pipeline_status(STATUS_FILE_PATH, generate_history_point)
-    jwt_token = get_jwt_token(URL_TOKEN)
-    header_data = {
-        "Authorization": f"Bearer {jwt_token}",
-        "If-Modified-Since": f"{defined_datetime_point}"
-    }
-
-    result = get_vacancy_data(URL_VACANCY, header_data)
-    save_vacancy_data(result)
-
-    logger.info("End ingestion process")
-
+    logger.info("Start ingestion process.")
+    defined_datetime_point = get_pipeline_status(generate_history_point)
+    jwt_token = get_jwt_token()
+    fetch_and_save_vacancy_data(jwt_token, defined_datetime_point)
+    logger.info("End ingestion process.")
 
 if __name__ == "__main__":
     run_ingestion()
