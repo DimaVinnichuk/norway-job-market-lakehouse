35ee38334aa728eb321960ea1c94ab5c07527a97
DimaVinnichuk <112745893+DimaVinnichuk@users.noreply.github.com>
2026-06-21 01:09:05 +0200

    Feature/ingestion (#1)
    * feat(ingest): progress on ingest script development
    
    - Add configuration file and setup logging
    - Update .gitignore to exclude local data files and logs
    - Add timestamp creation logic to determine pipeline status
    - Implement logic to automatically determine vacancy data ingestion method based on pipeline state
    - Add logic to fetch public token for vacancy data requests
    - Implement vacancy data fetching logic
    - Add logic to save vacancy data into a file
    - Add pipeline execution logic
    - TODO: Finalize data extraction logic to fetch full-size data and add validation
    - TODO: Implement saving each data page into a separate file
    
    * feat(ingest): implement vacancy ingestion and pagination pipeline
    
    - Refactor function arguments to leverage global constants
    - Implement dynamic pagination loop for automated vacancy extraction
    - Ensure robust error handling and explicit exit codes upon failures
    - Save atomic incremental state info upon successful execution

---
+++ ingest.py
@@ -0,0 +1,123 @@
+from datetime import datetime, timezone, timedelta
+from pathlib import Path
+from logging_config import setup_logging
+import logging
+import requests
+import json
+from urllib.parse import urljoin
+
+setup_logging()
+logger = logging.getLogger(__name__)
+
+URL_TOKEN = "https://pam-stilling-feed.nav.no/api/publicToken"
+URL_VACANCY = "https://pam-stilling-feed.nav.no/api/v1/feed"
+DOMAIN = "https://pam-stilling-feed.nav.no/"
+
+STATUS_FILE_PATH = Path("data/pipeline_state.txt")
+
+def generate_history_point():
+    """Creates a timestamp for 30 days ago, which will be the starting point for populating historical data """
+
+    logger.debug("Creating historical date point...")
+    today = datetime.now(timezone.utc)
+    thirty_days_ago = (today - timedelta(days=30)).strftime("%a, %d %b %Y %H:%M:%S GMT")
+    logger.debug("Created date point: %s", thirty_days_ago)
+    return thirty_days_ago
+
+def get_pipeline_status(get_date):
+    """Checks for pipeline status file existence, reading its data or falling back to default."""
+
+    logger.info("Checking pipeline execution status...")
+    if STATUS_FILE_PATH.exists():
+        logger.info("Pipeline status file found at %s. Reading existing state.", STATUS_FILE_PATH)
+        content = STATUS_FILE_PATH.read_text(encoding="utf-8").strip()
+        logger.debug("Retrieved date from status file: %s", content)
+        return content
+    else:
+        logger.info("Pipeline status file NOT found at %s. Triggering historical backfill mode.", STATUS_FILE_PATH)
+        backfill_point = get_date()
+        return backfill_point
+
+def get_jwt_token():
+    """Requests and returns a public token from the NAV API"""
+
+    try:
+        logger.info("Requesting public token...")
+        token_response = requests.get(URL_TOKEN, timeout=10)
+        token_response.raise_for_status()
+        try:
+            token = token_response.text.split(":")[-1].strip()
+            if (len(token.split(".")) == 3):
+                logger.info("Public token successfully retrieved.")
+                return token
+            else: 
+                raise ValueError("Invalid JWT token structure received from API")
+        except ValueError as e:
+            logger.critical("Token parsing error %s", e)
+            raise SystemExit(1)
+
+    except requests.exceptions.RequestException as e:
+        logger.critical("Error retrieving token: %s.", e)
+        raise SystemExit(1)
+
+def fetch_and_save_vacancy_data(token, time_point):
+    """Fetch and save vacancy data"""
+
+    header_data = {
+        "Authorization": f"Bearer {token}",
+        "If-Modified-Since": f"{time_point}"
+    }
+    logger.info("Fetching vacancy data from API...")
+    start_timestamp = datetime.now(timezone.utc)
+    timestamp_filename = start_timestamp.strftime("%d%m%y-%H%M%S")
+    timestamp_for_update_state = start_timestamp.strftime("%a, %d %b %Y %H:%M:%S GMT")
+    page_url = URL_VACANCY
+    page_number = 1
+
+    while page_url:
+        data_filename = f"{timestamp_filename}-page{page_number}-raw_jobs.json"
+        path_to_data = Path("data/bronze/") / data_filename
+        path_to_data.parent.mkdir(parents=True, exist_ok=True)
+
+        try:
+            logger.debug("Requesting URL: %s...", page_url)
+            response = requests.get(page_url, headers=header_data, timeout=10)
+            response.raise_for_status()
+            data_json = response.json()
+        except requests.exceptions.RequestException as e:
+            logger.critical("Failed to retrieve data from API: %s.", e)
+            raise SystemExit(1)
+        
+        if data_json.get("items"):
+            next_url = data_json.get("next_url")
+            with open(path_to_data, "w", encoding="utf-8") as f:
+                json.dump(data_json, f, indent=4)
+            logger.debug("Retrieved page saved to %s.", path_to_data)
+            page_number+=1
+            if next_url:
+                page_url = urljoin(DOMAIN, next_url)
+            else:
+                logger.info("Next URL is missing. Pagination finished.")
+                break
+        else:
+            logger.info("No vacancy data found in the response")
+            break
+
+    logger.info("Vacancy data ingestion completed successfully.")
+    STATUS_FILE_PATH.parent.mkdir(parents=True, exist_ok=True)
+    STATUS_FILE_PATH.write_text(timestamp_for_update_state, encoding="utf-8")
+    logger.debug("Pipeline state file updated with timestamp: %s", timestamp_for_update_state)
+
+### main execution
+
+def run_ingestion():
+    """Launch ingestion process"""
+
+    logger.info("Start ingestion process.")
+    defined_datetime_point = get_pipeline_status(generate_history_point)
+    jwt_token = get_jwt_token()
+    fetch_and_save_vacancy_data(jwt_token, defined_datetime_point)
+    logger.info("End ingestion process.")
+
+if __name__ == "__main__":
+    run_ingestion()
