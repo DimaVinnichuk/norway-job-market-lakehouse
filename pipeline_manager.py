b72ff1d7e28d8a6ce7118684152b002ad6c3159c
DimaVinnichuk <1dima.vinnichuk@gmail.com>
2026-06-29 17:40:44 +0200

    feat(orchestration): implement state recovery, batching, and directory cleanup
    - Add disaster recovery mode to orchestrator to resume failed runs using run timestamps.
    
    - Implement empty directory cleanup within the orchestrator to prevent stalled pipelines.
    
    - Add response handling for empty feed updates (API 404 responses).
    
    - Refactor job details ingestion to support date-based partitioning and batching logic.
    
    - Introduce helper function for batch writing and refactor variables for naming consistency.

--- pipeline_manager.py
+++ pipeline_manager.py
@@ -1,8 +1,9 @@
 from src.ingest import generate_history_point, get_jwt_token, \
-    get_pipeline_status, fetch_and_save_feed_data, fetch_and_save_vacancy_details, generate_header_data
+    get_start_date, ingest_feed_data, ingest_job_details, generate_header_data
 from logging_config import setup_logging
 import logging
-from src.constants import BRONZE_FEED_DIR, STATE_FILE, ACTIVE_VACANCY_DETAILS_DIR, INACTIVE_VACANCY_DETAILS_DIR
+from datetime import datetime, timezone
+from src.constants import BRONZE_FEED_DIR, STATE_FILE, ACTIVE_JOB_DIR, INACTIVE_JOB_DIR
 
 setup_logging()
 logger = logging.getLogger("__name__")
@@ -10,13 +11,47 @@ logger = logging.getLogger("__name__")
 def run_feed_ingestion():
     """Launch feed ingestion process"""
 
-    logger.info("Start ingestion process.")
-    last_run_date = get_pipeline_status(generate_history_point, STATE_FILE)
-    jwt_token = get_jwt_token()
-    header_data = generate_header_data(jwt_token, last_run_date)
-    fetch_and_save_feed_data(header_data, BRONZE_FEED_DIR, STATE_FILE)
-    fetch_and_save_vacancy_details(BRONZE_FEED_DIR, ACTIVE_VACANCY_DETAILS_DIR, INACTIVE_VACANCY_DETAILS_DIR, last_run_date, header_data)
-    logger.info("End ingestion process.")
+    logger.info("Pipeline start.")
+
+    token = get_jwt_token()
+    state_file_content = get_start_date(generate_history_point, STATE_FILE)
+    header_data = generate_header_data(token, state_file_content)
+    current_date = datetime.now(timezone.utc)
+
+    incomplited_run_dir = None
+    if BRONZE_FEED_DIR.exists():
+        state_date = datetime.strptime(state_file_content, "%a, %d %b %Y %H:%M:%S GMT").replace(tzinfo=timezone.utc)
+        dirs = list(BRONZE_FEED_DIR.glob("run-*"))
+        if dirs:
+            dir_date = None
+            for dir in dirs:
+                run_files = list(dir.glob("*.json"))
+                if not run_files:
+                    logger.debug("Empty dir %s removed", dir)
+                    dir.rmdir()
+                    continue
+                date_from_dir = datetime.strptime(dir.name.strip(), "run-%d%m%y-%H%M%S").replace(tzinfo=timezone.utc)
+                if date_from_dir > state_date:
+                    incomplited_run_dir = dir
+                    dir_date = date_from_dir
+                    
+    if incomplited_run_dir:
+        logger.info("Found incomplited session folder: %s Start emergency ingestion", incomplited_run_dir)
+        ingest_job_details(dir_date, incomplited_run_dir, ACTIVE_JOB_DIR, INACTIVE_JOB_DIR, header_data, STATE_FILE)
+        
+        pipeline_run_date = current_date.strftime("%a, %d %b %Y %H:%M:%S GMT")
+        STATE_FILE.write_text(pipeline_run_date, encoding="utf-8")
+        logger.info("Pipeline state file updated wit %s", pipeline_run_date)
+    else:
+        logger.info("Default ingestion")
+        fresh_feed_dir = ingest_feed_data(current_date, header_data, BRONZE_FEED_DIR)
+        ingest_job_details(current_date, fresh_feed_dir, ACTIVE_JOB_DIR, INACTIVE_JOB_DIR, header_data, STATE_FILE)
+
+        pipeline_run_date = current_date.strftime("%a, %d %b %Y %H:%M:%S GMT")
+        STATE_FILE.write_text(pipeline_run_date, encoding="utf-8")
+        logger.info("Pipeline state file updated wit %s", pipeline_run_date)
+
+    logger.info("Pipeline successfuly ended.")
 
 if __name__ == "__main__":
     run_feed_ingestion()
--- src/ingest.py
+++ src/ingest.py
@@ -4,6 +4,7 @@ import requests
 import json
 from urllib.parse import urljoin
 import time
+from pathlib import Path
 
 logger = logging.getLogger(__name__)
 
@@ -12,10 +13,12 @@ URL_FEED = "https://pam-stilling-feed.nav.no/api/v1/feed"
 URL_DETAILS = "https://pam-stilling-feed.nav.no/api/v1/feedentry/"
 DOMAIN = "https://pam-stilling-feed.nav.no/"
 
-def generate_header_data(token, time_point):
+def generate_header_data(token, str_time_point):
+    """Generate header data for API request"""
+
     head = {
         "Authorization": f"Bearer {token}",
-        "If-Modified-Since": f"{time_point}",
+        "If-Modified-Since": f"{str_time_point}",
         "Accept": "application/json"
     }
     return head
@@ -29,14 +32,13 @@ def generate_history_point():
     logger.debug("Created date point: %s", thirty_days_ago)
     return thirty_days_ago
 
-def get_pipeline_status(generate_history_point, state_file):
-    """Checks for pipeline status file existence, reading its data or falling back to default."""
-
-    logger.info("Checking pipeline execution status...")
+def get_start_date(generate_history_point, state_file):
+    """Checks for pipeline state file existence, reading its data or falling back to default."""
+    
     if state_file.exists():
-        logger.info("Pipeline status file found at %s. Reading existing state.", state_file)
+        logger.info("Pipeline state file found at %s. Reading existing state.", state_file)
         content = state_file.read_text(encoding="utf-8").strip()
-        logger.debug("Retrieved date from status file: %s", content)
+        logger.debug("Retrieved date from state file: %s", content)
         return content
     else:
         logger.info("Pipeline status file NOT found at %s. Triggering historical backfill mode.", state_file)
@@ -50,143 +52,183 @@ def get_jwt_token():
         logger.info("Requesting public token...")
         token_response = requests.get(URL_TOKEN, timeout=10)
         token_response.raise_for_status()
-        try:
-            token = token_response.text.split(":")[-1].strip()
-            if (len(token.split(".")) == 3):
-                logger.info("Public token successfully retrieved.")
-                return token
-            else: 
-                raise ValueError("Invalid JWT token structure received from API")
-        except ValueError as e:
-            logger.critical("Token parsing error %s", e)
-            raise SystemExit(1)
-
     except requests.exceptions.RequestException as e:
         logger.critical("Error retrieving token: %s.", e)
         raise SystemExit(1)
+    try:
+        token = token_response.text.split(":")[-1].strip()
+        if (len(token.split(".")) == 3):
+            logger.info("Public token successfully retrieved.")
+            return token
+        else: 
+            raise ValueError("Invalid JWT token structure received from API")
+    except ValueError as e:
+        logger.critical("Token parsing error %s", e)
+        raise SystemExit(1)
 
-def fetch_and_save_feed_data(header_data, bronze_feed_dir, state_file):
-    """Fetch and save feed data"""
+def ingest_feed_data(pipeline_run_date, header_data, bronze_feed_dir):
+    """Extract and feed data and return curent run dir path"""
 
-    logger.info("Fetching vacancy feed data from API...")
-    start_timestamp = datetime.now(timezone.utc)
-    timestamp_filename = start_timestamp.strftime("%d%m%y-%H%M%S")
-    timestamp_for_update_state = (start_timestamp - timedelta(hours=3)).strftime("%a, %d %b %Y %H:%M:%S GMT")
+    logger.info("Ingesting feed data from API...")
+    timestamp_filename = pipeline_run_date.strftime("%d%m%y-%H%M%S")
+    current_run_dir = bronze_feed_dir / f"run-{timestamp_filename}"
+    current_run_dir.mkdir(parents=True, exist_ok=True)
+        
     page_url = URL_FEED
     page_number = 1
 
     while page_url:
-        data_filename = f"{timestamp_filename}-page{page_number}-raw_jobs.json"
-        path_to_data = bronze_feed_dir / data_filename
-        path_to_data.parent.mkdir(parents=True, exist_ok=True)
-
+        filename = f"{timestamp_filename}-page{page_number}-raw-jobs.json"
+        path_to_data = current_run_dir / filename
         try:
             data_json = None
-            # logger.debug("Requesting URL: %s...", page_url)
             response = requests.get(page_url, headers=header_data, timeout=10)
-            response.raise_for_status()
-            try:
-                data_json = response.json()
-                # logger.debug("Response content parsed to json successfuly")
-            except json.JSONDecodeError as e:
-                logger.critical("Failed to parse response content to json. Error: %s", e)
+            if response.status_code == 404:
+                logger.info("No fresh data in feed. Response status code: 404")
+                logger.info("Feed data ingestig proccess ended")
                 raise SystemExit(1)
+            response.raise_for_status()
         except requests.exceptions.RequestException as e:
             logger.critical("Failed to retrieve data from API: %s.", e)
             raise SystemExit(1)
-        
-        if data_json.get("items"):
-            with open(path_to_data, "w", encoding="utf-8") as f:
-                json.dump(data_json, f, indent=4)
-            # logger.debug("Retrieved data saved to %s", path_to_data)
-            page_number+=1
-            new_url = None
-            next_url = data_json.get("next_url")
-            if next_url:
-                new_url = urljoin(DOMAIN, next_url)
-            else:
-                logger.info("Next URL is missing. Pagination finished.")
-                break
-            if new_url != page_url:
-                page_url = new_url
-                # logger.debug("Next page URL: %s", page_url)
-            else:
-                logger.critical("Next URL is equal to past one. Validation failed")
-                break        
+        try:
+            data_json = response.json()
+        except json.JSONDecodeError as e:
+            logger.critical("Failed to parse response content to json. Error: %s", e)
+            raise SystemExit(1)
+        with open(path_to_data, "w", encoding="utf-8") as f:
+            json.dump(data_json, f, indent=4)
+        page_number+=1
+        new_url = None
+        next_url = data_json.get("next_url")
+        if next_url:
+            new_url = urljoin(DOMAIN, next_url)
         else:
-            logger.info("No vacancy data found in the response")
+            logger.info("Next URL is missing. Pagination finished.")
             break
-        time.sleep(1)
-
-    logger.info("Vacancy data ingestion completed successfully.")
-    state_file.parent.mkdir(parents=True, exist_ok=True)
-    state_file.write_text(timestamp_for_update_state, encoding="utf-8")
-    logger.debug("State file created/updated, at: %s", state_file)
-    logger.debug("Pipeline state file updated with timestamp: %s", timestamp_for_update_state)
-
-def fetch_and_save_vacancy_details(feed_dir, active_dir, inactive_dir, date_start_point, header_data):
-
-    logger.info("Start fetching and saving vacancy data details proccess.")
-    active_dir.mkdir(parents=True, exist_ok=True)
-    inactive_dir.mkdir(parents=True, exist_ok=True)
-
-    feed_files = feed_dir.glob("*.json")
-
-    last_run_date = datetime.strptime(date_start_point, "%a, %d %b %Y %H:%M:%S GMT")
-    last_run_date = last_run_date.replace(tzinfo=timezone.utc)
-
-    uniq_ids = set()
-    
-    for feed_file in feed_files:
-        file_modif_date = datetime.fromtimestamp(feed_file.stat().st_mtime, tz=timezone.utc)
-        if file_modif_date <= last_run_date:
-            continue
+        if new_url != page_url:
+            page_url = new_url
         else:
-            with open(feed_file, "r", encoding="utf-8") as f:
-                try:
-                    feed_file_data = json.load(f)
-                except json.JSONDecodeError as e:
-                    logger.warning("Skip file %s. Do not contains valid json. Error: %s", feed_file, e)
-                feed_items = feed_file_data.get("items")
-                if feed_items:
-                    for item in feed_items:
-                        item_id = item.get("id").strip()
-                        if not item_id:
-                            logger.warning("Vacancy from file %s skipped. Not valid id", feed_file)
-                        else:
-                            uniq_ids.add(item_id)
-                else:
-                    logger.warning("Skip file %s. Feed file do not contains any vacancy data.", feed_file)
-    logger.info(f"{len(uniq_ids)} new changes was found.")
-
-    logger.info("Fetching vacancy details and changes...")
-    for vacancy_id in uniq_ids:
-        if vacancy_id:
-            url_details = urljoin(URL_DETAILS, vacancy_id)
-            try:
-                response = requests.get(url_details, headers=header_data, timeout=10)
-                response.raise_for_status()
-            except requests.RequestException as e:
-                logger.critical("Failed to retrieve data details from API: %s", e)
-                raise SystemExit(1)
-            try:
-                vacancy_details_data = response.json()
-            except json.JSONDecodeError as e:
-                logger.warning("Feil during parsing response content for id: %s. Not valid json in response", vacancy_id)
-            vacancy_status = vacancy_details_data.get("status").strip()
-            filename = f"{vacancy_id}_{vacancy_status}.json"
-            if vacancy_status == "ACTIVE":
-                filepath = active_dir / filename
-            else:
-                filepath = inactive_dir / filename
-            with open(filepath, "w", encoding="utf-8") as f:
-                json.dump(vacancy_details_data, f, indent=4)        
-                     
-        else:
-            logger.warning("Not valid id. Skipped")
-            continue
+            logger.critical("Next URL is equal to past one. Validation failed")
+            break
         time.sleep(0.5)
-    logger.info("End fetching and saving details data proccess.")
+    logger.info("Feed data ingestion completed successfully.")
+    return current_run_dir
+
+def ingest_job_details(dirs_name_date, fresh_feed_data_dir, active_dir, inactive_dir, header_data, pipeline_state_file):
+    """Extract and save job details from API"""
+
+    def write_data_to_file(mode, data, batch_num, partition):
+        filename = f"{dir_time}_batch{batch_num}.json"
+        filepath = partition / filename
+        with open(filepath, mode, encoding="utf-8")as f:
+            json.dump(data, f, indent=4)
+        return batch_num + 1
+    
+    active_batch_num = 1
+    inactive_batch_num = 1
+
+    partition = Path(f"{dir_year}/{dir_month}/{dir_day}")
+    active_partition_path = active_dir / partition
+    inactive_partition_path = inactive_dir / partition
+
+    writed_job_ids = None
+
+    if active_partition_path.exists():
+        active_jobs_batches = list(active_partition_path.glob("*.json"))
+        if active_jobs_batches:
+            active_batch_num = len(active_jobs_batches)
+            for active_job_batch in active_jobs_batches:
+                for job in active_job_batch:
+                    job_id = job.get("uuid")
+                    if job_id:
+                        writed_job_ids.add()
+                    else:
+                        logger.warning("Missed uuid for job in batch %s", active_job_batch)
+        else:
+            active_partition_path.rmdir()
+            logger.info("Empty path %s, removed", active_partition_path)
+            
+
+
+    logger.info("Ingesting job details...")
+    logger.debug("Reading fresh feed data dir: %s...", fresh_feed_data_dir)
+
+    feed_files = list(fresh_feed_data_dir.glob("*.json"))
+    if fresh_feed_data_dir.exists() and not feed_files:
+        fresh_feed_data_dir.rmdir()
+        logger.debug("Empty directory %s cleaned", fresh_feed_data_dir)
+        logger.info("No json data files found at %s. Job details ingestion finished", fresh_feed_data_dir)
+        raise SystemExit(1)
+    
+    logger.debug("Feed data found at: %s", fresh_feed_data_dir)
+    logger.debug("Creating unique ids from fresh feed data for request details")
+    uniq_ids = set()
+    for feed_file in feed_files:
+        with open(feed_file, "r", encoding="utf-8") as f:
+            try:
+                feed_file_data = json.load(f)
+            except json.JSONDecodeError as e:
+                logger.warning("Skip file %s. Do not contains valid json. Error: %s", feed_file, e)
+                continue
+            feed_items = feed_file_data.get("items")
+            if feed_items:
+                for item in feed_items:
+                    item_id = item.get("id").strip()
+                    if not item_id:
+                        logger.warning("Vacancy from file %s skipped. Not valid id", feed_file)
+                    else:
+                        uniq_ids.add(item_id)
+            else:
+                logger.warning("Skip file %s. Feed file do not contains any job data.", feed_file)
+    logger.info(f"{len(uniq_ids)} new changes in feed was found.")
+
+    dir_year = dirs_name_date.strftime("%Y")
+    dir_month = dirs_name_date.strftime("%m")
+    dir_day = dirs_name_date.strftime("%d")
+    dir_time = dirs_name_date.strftime("%H%M%S")
+
+    inactive_partition_path.mkdir(parents=True, exist_ok=True)
+    active_partition_path.mkdir(parents=True, exist_ok=True)
+
+    active_job_list = []
+    inactive_job_list = []
+
+    for job_id in uniq_ids:
+        url_details = urljoin(URL_DETAILS, job_id)
+        try:
+            response = requests.get(url_details, headers=header_data, timeout=10)
+            response.raise_for_status()
+        except requests.RequestException as e:
+            logger.critical("Failed to retrieve data details from API: %s", e)
+            raise SystemExit(1)
+        try:
+            job_details = response.json()
+        except json.JSONDecodeError as e:
+            logger.warning("Skippeed job details for id: %s. Error: %s", job_id, e)
+            continue
+
+        job_status = job_details.get("status").strip()
+        if job_status:
+            if job_status == "ACTIVE":
+                active_job_list.append(job_details)
+                if len(active_job_list) >= 100:
+                    active_batch_num = write_data_to_file("w", active_job_list, active_batch_num, active_partition_path)
+                    active_job_list.clear()
+            else:
+                inactive_job_list.append(job_details)
+                if len(inactive_job_list) >= 1000:
+                    inactive_batch_num = write_data_to_file("w", inactive_job_list, inactive_batch_num, inactive_partition_path) 
+                    inactive_job_list.clear()
+        else:
+            logger.warning("Job details respons has no status fiels. ID: %s skipped", job_id)      
+    time.sleep(0.5)
+
+    if active_job_list:
+        write_data_to_file("w", active_job_list, active_batch_num, active_partition_path)
+    if inactive_job_list:
+        write_data_to_file("w", inactive_job_list, inactive_batch_num, inactive_partition_path)
+    logger.info("End ingesting job details proccess.")
 
 
     
